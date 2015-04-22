436c2ea4ca77ac41f4fa4fb995aa4d8131203908
Simon Hengel <sol@typeful.net>
2015-04-22 12:07:43 +0800

    Depend on mockery

--- test/Helper.hs
+++ test/Helper.hs
@@ -1,18 +1,10 @@
 module Helper (
   module Test.Hspec
 , module Control.Applicative
-, inTempDirectory
+, module Test.Mockery.Directory
 ) where
 
 import           Test.Hspec
+import           Test.Mockery.Directory
 import           Prelude.Compat
 import           Control.Applicative
-import           Control.Exception
-import           System.Directory
-import           System.IO.Temp
-
-inTempDirectory :: IO a -> IO a
-inTempDirectory action = withSystemTempDirectory "hspec" $ \path -> do
-  bracket getCurrentDirectory setCurrentDirectory $ \_ -> do
-    setCurrentDirectory path
-    action
