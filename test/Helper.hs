f90e7265760e540d028735f21289c85bd36d7fd0
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-20 22:16:50 +0800

    Delete test directory

--- test/Helper.hs
+++
@@ -1,9 +0,0 @@
-module Helper (
-  module Test.Hspec
-, module Control.Applicative
-, module Test.Mockery.Directory
-) where
-
-import           Test.Hspec
-import           Test.Mockery.Directory
-import           Control.Applicative
